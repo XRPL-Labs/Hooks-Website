542f4aececcd473e7e2be6f4d0be21c91a286836
Wietse Wind <mail@wietse.com>
2023-06-07 03:11:32 +0200

    Components v2»v3

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -75,7 +75,7 @@ export default {
   },
   data () {
     return {
-      endpoint: 'wss://hooks-testnet-v2.xrpl-labs.com'
+      endpoint: 'wss://hooks-testnet-v3.xrpl-labs.com'
     }
   },
   async mounted () {
